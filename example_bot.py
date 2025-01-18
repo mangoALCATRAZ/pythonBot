0b162d5f14f6bf51416a9e7a065111bfc6040188
Solidus Snake <snake@dontdox.me>
2025-01-18 16:43:57 -0500

    added discord.py dependancy and updated requirements.txt

--- example_bot.py
+++ example_bot.py
@@ -1,3 +1,20 @@
 import discord
 
-intents - discord.Intents
+# DOn't store this here, dingus!
+DISCORD_TOKEN = "MTMzMDI5MDE3MDc3NTIxMjE4NA.Gu_q7i.tY0m5qM2Q_7XU3Zg68tHkUe1iXFP9EpeVYvpZM"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.content == "!hello":
+        await message.channel.send(f'Hello, {message.author.mention}!')
+
+client.run("")
