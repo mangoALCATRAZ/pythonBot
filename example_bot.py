3f30687981e67b26a8024d18fb02f5d0e842b355
Solidus Snake <snake@dontdox.me>
2025-01-20 10:57:14 -0500

    added basic slap functionality

--- example_bot.py
+++ example_bot.py
@@ -1,21 +1,22 @@
 import discord
+from discord.ext import commands
 
 # DOn't store this here, dingus!
-DISCORD_TOKEN = "MTMzMDI5MDE3MDc3NTIxMjE4NA.Gu_q7i.tY0m5qM2Q_7XU3Zg68tHkUe1iXFP9EpeVYvpZM"
+DISCORD_TOKEN = "MTMzMDI5MDE3MDc3NTIxMjE4NA.Gn4F9X.ypNYhSTMCVWbnnR-bS09ewmk-QXgI7_jpadpbo"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.content == "!hello":
-        await message.channel.send(f'Hello, {message.author.mention}!')
+@bot.command()
+async def slap(ctx, who : discord.Member, withwhat):
+    nick = who.nick
+    me = ctx.author.nick
+    await ctx.send(f"{me} slapped {nick} with a {withwhat}")
 
-client.run(DISCORD_TOKEN)
+
+bot.run(DISCORD_TOKEN)
+
 print("Bot is connected to server")
